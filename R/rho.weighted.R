c23d8613399c1dcfe38f22f646a4d7e9de9f658f
Igor <miellago98@gmail.com>
2024-03-05 22:25:14 +0000

    first steps

---
+++ R/rho.weighted.R
@@ -0,0 +1,13 @@
+###################################################################
+##### MEAN RESULTANT VECTOR LENGTH FOR CIRCULAR OBSERVATIONS  #####
+##### WITH RANK WEIGHTS                                       #####
+#####                                                         #####
+##### Author: Igor Yegin                                      #####
+###################################################################
+
+rho.weighted <- function(theta, w) {
+  w <- rank(w)
+  a <- sum(w * cos(theta)) / sum(w)
+  b <- sum(w * sin(theta)) / sum(w)
+  sqrt(a^2 + b^2)
+}
---
+++ R/trig.moment.weighted.R
@@ -0,0 +1,18 @@
+###################################################################
+##### TRIGONOMETRIC MOMENTS (COSINE AND SINE)                 #####
+##### FOR RANK-WEIGHTED CIRCULAR OBSERVATIONS                 #####
+#####                                                         #####
+##### Author: Igor Yegin                                      #####
+###################################################################
+
+trig.moment.weighted <- function(theta, w, p = 1, central = FALSE) {
+  if(central) {
+    a <- sum(w * cos(theta)) / sum(w)
+    b <- sum(w * sin(theta)) / sum(w)
+    list(C = sum(w * cos(p * (theta - atan2(b, a)))) / sum(w),
+         S = sum(w * sin(p * (theta - atan2(b, a)))) / sum(w))
+  }
+  else {
+    list(C = sum(w * cos(p * theta)) / sum(w), S = sum(w * sin(p * theta)) / sum(w))
+  }
+}

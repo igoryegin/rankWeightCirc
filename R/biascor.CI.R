da7d8549dcd490eda51e705c4936f0f55754bffa
igy <miellago98@gmail.com>
2024-05-07 22:22:00 +0100

    some amendments to the output

--- R/biascor.CI.R
+++ R/biascor.CI.R
@@ -22,13 +22,17 @@ biascor.CI <- function(stat = c("mu", "rho"), theta, w, alpha) {
   rhobar <- sqrt(a^2 + b^2)
   if(stat == "rho") {
     rhocorr <- rhobar - (2 * n + 1) / (3 * n + 3) * (1 - a2bar) / (2 * n * rhobar)
-    rhosd <- qnorm(1 - alpha/2) * sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - 2 * rhobar ^ 2 + a2bar))
-    list(rho = rhocorr, stdev = rhosd, lower = rhocorr - rhosd, upper = rhocorr + rhosd)
+    rhosd <- sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - 2 * rhobar ^ 2 + a2bar))
+    list(rho = rhocorr, stdev = rhosd,
+         lower = rhocorr - qnorm(1 - alpha/2) * rhosd,
+         upper = rhocorr + qnorm(1 - alpha/2) * rhosd)
   }
   else if(stat == "mu") {
     mucorr <- mu - (2 * n + 1) / (3 * n * (n + 1)) * b2bar / rhobar ^ 2
-    musd <- qnorm(1 - alpha/2) * sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - a2bar) / rhobar ^ 2)
-    list(mu = mucorr, stdev = musd, lower = mucorr - musd, upper = mucorr + musd)
+    musd <- sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - a2bar) / rhobar ^ 2)
+    list(mu = mucorr, stdev = musd,
+         lower = mucorr - qnorm(1 - alpha/2) * musd,
+         upper = mucorr + qnorm(1 - alpha/2) * musd)
   }
   else
     stop("Incorrect argument name. 'stat' must be either 'mu' or 'rho'")

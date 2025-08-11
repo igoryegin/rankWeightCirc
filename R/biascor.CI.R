8c783aca69c4e1d52767dda565388b9d01404911
igy <miellago98@gmail.com>
2025-08-12 00:01:03 +0100

    rewrote the algebraic expressions in biascor.CI()

--- R/biascor.CI.R
+++ R/biascor.CI.R
@@ -8,7 +8,7 @@
 #                                                         #
 ###########################################################
 
-biascor.CI <- function(stat = c("mu", "rho"), theta, w, CI.level) {
+biascor.CI <- function(stat = c("mu", "rho"), theta, w, CI.level = 0.95) {
   stat <- match.arg(stat)
   w <- rank(w, ties.method = "first")
   n <- length(theta)
@@ -21,14 +21,14 @@ biascor.CI <- function(stat = c("mu", "rho"), theta, w, CI.level) {
   mu <- atan2(b, a)
   rhobar <- sqrt(a^2 + b^2)
   if(stat == "rho") {
-    rhocorr <- rhobar - (2 * n + 1) / (3 * n + 3) * (1 - a2bar) / (2 * n * rhobar)
+    rhocorr <- rhobar - (4 * n + 2) / (3 * n + 3) * (1 - a2bar) / (4 * n * rhobar)
     rhosd <- sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - 2 * rhobar ^ 2 + a2bar))
     list(rho = rhocorr, stdev = rhosd,
          lower = rhocorr - qnorm(1/2 + CI.level/2) * rhosd,
          upper = rhocorr + qnorm(1/2 + CI.level/2) * rhosd)
   }
   else if(stat == "mu") {
-    mucorr <- mu + (2 * n + 1) / (3 * n * (n + 1)) * b2bar / rhobar ^ 2
+    mucorr <- mu + (4 * n + 2) / (3 * n + 3) * b2bar / (2 * n * rhobar ^ 2)
     musd <- sqrt((2 * n + 1) / (3 * n * (n + 1)) * (1 - a2bar) / rhobar ^ 2)
     list(mu = mucorr, stdev = musd,
          lower = mucorr - qnorm(1/2 + CI.level/2) * musd,
